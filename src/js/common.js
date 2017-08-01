20b3da865451367bed4750bc382399cf220da385
vasilisa <dr.mandarine@gmail.com>
2017-08-01 18:07:15 +0400

    added gradient bg to multidisciplinary

--- src/js/common.js
+++ src/js/common.js
@@ -445,7 +445,6 @@ var designInit = function (p) {
     }
     mConstraint = MouseConstraint.create(engine, options);
     World.add(world, mConstraint);
-    console.log(mConstraint);
 
     for (var i = 0; i < 15; i++) {
       boxes.push(new drawStack(Common.random(0, p.windowWidth), 0, Common.random(40, 300), Common.random(40, 300)));
@@ -497,6 +496,125 @@ var designInit = function (p) {
       p.rect(0, 0, this.w, this.h);
       p.pop();
     }
+  }
+}
+
+// Multidisciplinary
+// ----------------------------------------------------------------------------
+var multidisciplinaryInit = function (p) {
+  var $canvas
+  var $title
+
+  var Engine = Matter.Engine;
+      Common = Matter.Common,
+      World = Matter.World,
+      Bodies = Matter.Bodies,
+      Mouse = Matter.Mouse,
+      MouseConstraint = Matter.MouseConstraint;
+
+  var engine;
+  var world;
+  var gradientStart, gradientEnd;
+
+  p.setup = function () {
+    $canvas = p.createCanvas(p.windowWidth, (p.windowHeight))
+    $canvas.position(0, 0)
+    $title = p.createElement('h1', 'Multidisciplinary')
+    $title.position(0, 0)
+    $title.addClass('title')
+
+    gradientStart = p.color(255, 152, 151)
+    gradientEnd = p.color(246, 80, 160)
+
+    engine = Engine.create()
+    world = engine.world
+    Engine.run(engine)
+
+    var opts = {
+      isStatic: true
+    }
+
+    // walls
+    World.add(world, [
+      Bodies.rectangle(p.width, p.height/2, 1, p.height, opts),
+      Bodies.rectangle(p.width/2, p.height, p.width, 1, opts),
+      Bodies.rectangle(0, p.height/2, 1, p.height, opts)
+    ]);
+
+    var canvasmouse = Mouse.create($canvas.elt);
+    canvasmouse.pixelRatio = p.pixelDensity();
+    var options = {
+      mouse: canvasmouse
+    }
+    mConstraint = MouseConstraint.create(engine, options);
+    World.add(world, mConstraint);
+  }
+
+  p.draw = function () {
+    p.background('transparent');
+    setGradient(0, 0, p.width, p.height, gradientStart, gradientEnd);
+    Engine.update(engine);
+
+    for (var i = 0; i < boxes.length; i++) {
+      boxes[i].show();
+    }
+
+    if (mConstraint.body) {
+      var pos = mConstraint.body.position;
+      var offset = mConstraint.constraint.pointB;
+      var m = mConstraint.mouse.position;
+      p.stroke(0, 255, 0);
+      p.line(pos.x + offset.x, pos.y + offset.y, m.x, m.y);
+    }
+  }
+
+  p.windowResized = function() {
+    p.resizeCanvas(p.windowWidth, (p.windowHeight))
+  }
+
+  function setGradient(x, y, w, h, c1, c2) {
+    p.noFill();
+    for (var i = x; i <= x+w; i++) {
+      var inter = p.map(i, 0, w, 0, 1);
+      var c = p.lerpColor(c1, c2, inter);
+      p.stroke(c);
+      p.line(i, y, i, y+h);
+    }
+  }
+
+  function Particle(x, y, r, fixed) {
+    var options = {
+      friction: 0,
+      restitution: 0.95,
+      isStatic: fixed
+    }
+    this.body = Bodies.circle(x, y, r, options);
+    this.r = r;
+    World.add(world, this.body);
+
+    this.isOffScreen = function() {
+      var pos = this.body.position;
+      return (pos.y > height + 100);
+    }
+
+    this.removeFromWorld = function() {
+      World.remove(world, this.body);
+    }
+
+    this.show = function() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(1);
+      stroke(255);
+      fill(127);
+      ellipse(0, 0, this.r * 2);
+      pop();
+    }
+
   }
 }
 
@@ -570,6 +688,11 @@ page('/16', function (ctx, next) {
 
 page('/17', function (ctx, next) {
   var design = new p5(designInit, 'design')
+  next()
+})
+
+page('/20', function (ctx, next) {
+  var multidisciplinary = new p5(multidisciplinaryInit, 'multidisciplinary')
   next()
 })
 
