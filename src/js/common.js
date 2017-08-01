fde5e3cc9ef1972a850fcf84ae212c1b696b9137
Pavel Frolov <pfrlv@mail.ru>
2017-08-01 17:29:32 +0300

    up

--- src/js/common.js
+++ src/js/common.js
@@ -73,7 +73,6 @@ var graphicInit = function (p) {
 
   p.setup = function () {
     $title = p.createElement('h1', 'Graphic')
-    $title.position(0, 0)
     $title.addClass('title')
     $canvas = p.createCanvas(p.windowWidth + 150, p.windowHeight + 150)
     $canvas.position(-75, -75)
@@ -97,7 +96,6 @@ var strategyInit = function (p) {
 
   p.setup = function () {
     $title = p.createElement('h1', 'Strategy')
-    $title.position(0, 0)
     $title.addClass('title')
     $canvas = p.createCanvas(p.windowWidth, p.windowHeight)
   }
@@ -111,7 +109,6 @@ var interfaceInit = function (p) {
 
   p.setup = function () {
     $title = p.createElement('h1', 'Interface')
-    $title.position(0, 0)
     $title.addClass('title')
     $canvas = p.createCanvas(p.windowWidth, p.windowHeight)
   }
@@ -125,7 +122,6 @@ var uxInit = function (p) {
 
   p.setup = function () {
     $title = p.createElement('h1', 'User<br/>experience')
-    $title.position(0, 0)
     $title.addClass('title')
     $canvas = p.createCanvas(p.windowWidth, p.windowHeight)
   }
@@ -139,7 +135,6 @@ var threeDInit = function (p) {
 
   p.setup = function () {
     $title = p.createElement('h1', '3D graphics')
-    $title.position(0, 0)
     $title.addClass('title')
     $canvas = p.createCanvas(p.windowWidth, p.windowHeight)
   }
@@ -153,7 +148,6 @@ var artInit = function (p) {
 
   p.setup = function () {
     $title = p.createElement('h1', 'Art<br/>projects')
-    $title.position(0, 0)
     $title.addClass('title')
     $canvas = p.createCanvas(p.windowWidth, p.windowHeight)
   }
@@ -203,7 +197,6 @@ var identityInit = function (p) {
 
   p.setup = function () {
     $title = p.createElement('h1', 'Identity')
-    $title.position(0, 0)
     $title.addClass('title')
     $canvas = p.createCanvas(p.windowWidth, p.windowHeight)
   }
@@ -238,7 +231,6 @@ var motionInit = function (p) {
 
   p.setup = function () {
     $title = p.createElement('h1', 'Motion')
-    $title.position(0, 0)
     $title.addClass('title')
     p.createCanvas(p.windowWidth, p.windowHeight)
 
@@ -335,7 +327,6 @@ var illustrationInit = function (p) {
 
   p.setup = function () {
     $title = p.createElement('h1', 'Illustration')
-    $title.position(0, 0)
     $title.addClass('title')
     $canvas = p.createCanvas(p.displayWidth, p.displayHeight)
     $canvas.position(0, 0)
@@ -372,7 +363,6 @@ var identityInit = function (p) {
 
   p.setup = function () {
     $title = p.createElement('h1', 'Identity')
-    $title.position(0, 0)
     $title.addClass('title')
     $canvas = p.createCanvas(p.windowWidth + 150, p.windowHeight + 150)
     $canvas.position(-75, -75)
@@ -420,7 +410,6 @@ var designInit = function (p) {
     $canvas = p.createCanvas(p.windowWidth, (p.windowHeight))
     $canvas.position(0, 0)
     $title = p.createElement('h1', 'Product Design')
-    $title.position(0, 0)
     $title.addClass('title')
 
     engine = Engine.create()
@@ -435,6 +424,7 @@ var designInit = function (p) {
     World.add(world, [
       Bodies.rectangle(p.width, p.height/2, 1, p.height, opts),
       Bodies.rectangle(p.width/2, p.height, p.width, 1, opts),
+      Bodies.rectangle(p.width/2, 1, p.width, 1, opts),
       Bodies.rectangle(0, p.height/2, 1, p.height, opts)
     ]);
 
@@ -448,7 +438,7 @@ var designInit = function (p) {
     console.log(mConstraint);
 
     for (var i = 0; i < 15; i++) {
-      boxes.push(new drawStack(Common.random(0, p.windowWidth), 0, Common.random(40, 300), Common.random(40, 300)));
+      boxes.push(new drawStack(Common.random(0, p.windowWidth), 1, Common.random(40, 300), Common.random(40, 300)));
     }
   }
 
@@ -478,24 +468,26 @@ var designInit = function (p) {
       friction: 0.1,
       restitution: 0.1
     }
-    this.body = Bodies.rectangle(x, y, w, h, options);
-    this.w = w;
-    this.h = h;
-    this.color = p.random(colors);
-    World.add(world, this.body);
 
-    this.show = function() {
-      var pos = this.body.position;
-      var angle = this.body.angle;
+    this.body = Bodies.rectangle(x, y, w, h, options)
+    this.w = w
+    this.h = h
+    this.color = p.random(colors)
 
-      p.push();
-      p.translate(pos.x, pos.y);
-      p.rotate(angle);
-      p.rectMode(p.CENTER);
-      p.noStroke();
-      p.fill(this.color);
-      p.rect(0, 0, this.w, this.h);
-      p.pop();
+    World.add(world, this.body)
+
+    this.show = function () {
+      var pos = this.body.position
+      var angle = this.body.angle
+
+      p.push()
+      p.translate(pos.x, pos.y)
+      p.rotate(angle)
+      p.rectMode(p.CENTER)
+      p.noStroke()
+      p.fill(this.color)
+      p.rect(0, 0, this.w, this.h)
+      p.pop()
     }
   }
 }
@@ -639,3 +631,167 @@ page()
 //     }
 //   })
 // })()
+
+
+;(function () {
+  var canvas = document.getElementById('logo-canvas')
+  var ctx = canvas.getContext('2d')
+
+  function app1(p,a,c1,c2){
+    var l = Math.cos(a)*p[c1]+Math.sin(a)*p[c2]
+    var k = -Math.sin(a)*p[c1]+Math.cos(a)*p[c2]
+    p[c1] = l
+    p[c2] = k
+  }
+
+  function app2(p,a,c1,c2){
+    var l = Math.cos(a)*p[c1]-Math.sin(a)*p[c2]
+    var k = Math.sin(a)*p[c1]+Math.cos(a)*p[c2]
+    p[c1] = l
+    p[c2] = k
+  }
+
+  var _edges
+  function tesseractedges(){
+    if(!_edges){
+      var m = tesseractwithrotation(0,0,0,0,0,0)
+      var edges = []
+      var indicies = ['x','y','z','w']
+      for (var i = 0; i < m.length; i++) {
+        for (var j = i+1; j < m.length; j++) {
+          var count = 0
+          for (var k = 0; k < 4; k++) {
+            if (m[i][indicies[k]] === m[j][indicies[k]]) count++
+          };
+          if (count === 3) edges.push([i,j])
+        }
+      }
+      _edges = edges
+    }
+    return _edges 
+  }
+
+  function tesseractwithrotation(a,b,c,d,e,f) {
+    var verticies = []
+    for (var i = 0; i < 16; i++) {
+      var p = {
+        x: (i&1)*2 - 1,
+        y: ((i>>1)&1)*2 - 1,
+        z: ((i>>2)&1)*2 - 1,
+        w: ((i>>3)&1)*2 - 1
+      }
+      app1(p,a,'x','y')
+      app1(p,b,'y','z')
+      app1(p,c,'x','w')
+      app2(p,d,'x','z')
+      app2(p,e,'y','w')
+      app2(p,f,'z','w')
+      verticies.push(p)
+    }
+    return verticies
+  }
+
+  function project(point, size){
+    return {
+      x: (point.x + Math.SQRT2*point.z) * size,
+      y: (point.y + Math.SQRT2*point.w) * size
+    }
+  }
+
+  function drawtesseract(ctx, tesseract, opts){
+    var edges = tesseractedges()
+    for (var i = 0; i < tesseract.length; i++) {
+      var proj = project(tesseract[i], opts.size)
+      ctx.beginPath()
+      ctx.arc(proj.x + opts.x, proj.y + opts.y, opts.corner_radius, 0, 2 * Math.PI)
+      ctx.fill()
+    };
+    ctx.lineWidth = opts.line_width || 1
+    ctx.beginPath()
+    for (var i = 0; i < edges.length; i++) {
+      var v1 = project(tesseract[edges[i][0]], opts.size), 
+        v2 = project(tesseract[edges[i][1]], opts.size)
+      ctx.moveTo(v1.x+opts.x,v1.y+opts.y)
+      ctx.lineTo(v2.x+opts.x,v2.y+opts.y)
+    };
+    ctx.stroke()
+  }
+
+  var mouse = {
+    x: 0,
+    y: 0,
+    direction:0,
+
+    start: {
+      x:0,
+      y:0
+    },
+
+    dragging: false,
+
+    set: function (x,y) {
+      mouse.x = x
+      mouse.y = y
+      mouse.direction = Math.atan2(y-mouse.start.y,x-mouse.start.x)
+    },
+
+    coords: function (e) {
+      if(e.pageX){
+        mouse.set(e.pageX,e.pageY)
+      }
+      else if(e.offsetX) {
+        mouse.set(e.offsetX,e.offsetY)
+      }
+      else if(e.layerX) {
+        mouse.set(e.layerX,e.layerY)
+      }
+      else if(e.targetTouches && e.targetTouches.length > 0){
+        mouse.set(e.targetTouches[0].pageX,e.targetTouches[0].pageY)
+      }
+    },
+
+    down: function (e) {
+      mouse.coords(e)
+      mouse.start.x=mouse.x
+      mouse.start.y=mouse.y
+      mouse.dragging = true
+
+    },
+
+    up: function (e) {
+      mouse.coords(e)
+      mouse.dragging = false
+    }
+  }
+
+  document.addEventListener('mousemove', mouse.coords, true);
+
+  var gh = .14;
+
+  function main (time) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    var t = time/10000
+
+    ctx.strokeStyle = ctx.fillStyle = '#000'
+    var sm = 1
+
+    var m = tesseractwithrotation(t, t * 2, t * 3, mouse.x / 100, mouse.y / 100, 0)
+
+    drawtesseract(ctx, m, {
+      x: canvas.width / 2, 
+      y: canvas.height / 2, 
+      size: gh * canvas.height, 
+      line_width: 2,
+    })
+
+    lasttime = time
+    requestAnimationFrame(main)
+  }
+
+  requestAnimationFrame(function init(t) {
+    lasttime = t
+    requestAnimationFrame(main)
+  })
+
+})();
