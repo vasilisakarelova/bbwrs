4d6f0be6e39d9471ba52396c5aa4f6b88a2df654
Pavel Frolov <pfrlv@mail.ru>
2017-08-09 23:42:39 +0300

    up

--- build/js/common.js
+++ build/js/common.js
@@ -54529,58 +54529,64 @@ function uxInit(p) {
   var triangleShape, rectShape, circleShape, asterisk
   var triangle, rect, circle, draggedSprite, draggedSpriteType
 
+  var grey = '#adadad'
+
   p.setup = function () {
     $title = p.createElement('h1', 'User<br/>experience')
     $title.addClass('title')
     $canvas = p.createCanvas(p.windowWidth, p.windowHeight)
 
-    circleShape = p.createSprite(p.windowWidth / 2.2, p.windowHeight / 2, 50, 50)
+    circleShape = p.createSprite(p.windowWidth / 2.2, p.windowHeight / 1.5, 0, 0)
+    circleShape.setCollider('circle', 0, 0, 40)
+
     circleShape.draw = function() {
       p.push()
       p.noFill()
-      p.stroke(237,205,0)
+      p.stroke(grey)
       p.ellipse(0,0, 160, 160)
       p.pop()
     }
 
-    circle = p.createSprite(p.windowWidth / 1.5, p.windowHeight / 1.3)
+    circle = p.createSprite(p.random(160, p.windowWidth - 160), p.random(160, p.windowHeight - 160), 160, 160)
+    circle.setCollider('circle', 0, 0, 80)
+
     circle.draw = function() {
       p.push()
-      p.fill(237,205,0)
+      p.fill('#1ebc1e')
       p.ellipse(0,0, 160, 160)
       p.pop()
     }
 
-    triangleShape = p.createSprite(p.windowWidth / 4, p.windowHeight / 2, 50, 50)
+    triangleShape = p.createSprite(p.windowWidth / 4, p.windowHeight / 1.5, 100, 100)
     triangleShape.draw = function() {
       p.push()
       p.noFill()
-      p.stroke(255, 152, 151)
+      p.stroke(grey)
       p.triangle(-80, 80, 0, -80, 80, 80)
       p.pop()
     }
 
-    triangle = p.createSprite(p.windowWidth / 2.2, p.windowHeight / 1.3, 160, 160)
+    triangle = p.createSprite(p.random(80, p.windowWidth - 80), p.random(80, p.windowHeight - 80), 100, 100)
     triangle.draw = function() {
       p.push()
-      p.fill(255, 152, 151)
+      p.fill('#417ce6')
       p.triangle(-80, 80, 0, -80, 80, 80)
       p.pop()
     }
 
-    rectShape = p.createSprite(p.windowWidth / 1.5, p.windowHeight / 2, 50, 50)
+    rectShape = p.createSprite(p.windowWidth / 1.5, p.windowHeight / 1.5, 100, 100)
     rectShape.draw = function() {
       p.push()
       p.noFill()
-      p.stroke(246, 80, 160)
+      p.stroke(grey)
       p.rect(0, 0, 160, 160)
       p.pop()
     }
 
-    rect = p.createSprite(p.windowWidth / 4, p.windowHeight / 1.3)
+    rect = p.createSprite(p.random(160, p.windowWidth - 160), p.random(160, p.windowHeight - 160), 100, 100)
     rect.draw = function() {
       p.push()
-      p.fill(246, 80, 160)
+      p.fill('#ff3600')
       p.rect(0, 0, 160, 160)
       p.pop()
     }
@@ -54634,41 +54640,69 @@ function uxInit(p) {
     if (draggedSprite != null) {
       switch (draggedSpriteType) {
         case 'rect':
-          draggedSprite.position.x = p.mouseX
-          draggedSprite.position.y = p.mouseY
+          draggedSprite.velocity.x = (p.mouseX - draggedSprite.position.x) * 0.2;
+          draggedSprite.velocity.y = (p.mouseY - draggedSprite.position.y) * 0.2;
+
           draggedSprite.collide(circleShape)
           draggedSprite.collide(triangleShape)
-          if(draggedSprite.overlap(rectShape)) {
-            draggedSprite.position.x = p.windowWidth / 1.5
-            draggedSprite.position.y = p.windowHeight / 2
+
+          draggedSprite.overlap(rectShape, function (e) {
+            e.velocity.x = 0
+            e.velocity.y = 0
+
+            e.position.y = rectShape.position.y
+            e.position.x = rectShape.position.x
+
             draggedSprite = null
-          }
+          })
+
           break
         case 'triangle':
-          draggedSprite.position.x = p.mouseX
-          draggedSprite.position.y = p.mouseY
+          draggedSprite.velocity.x = (p.mouseX - draggedSprite.position.x) * 0.2;
+          draggedSprite.velocity.y = (p.mouseY - draggedSprite.position.y) * 0.2;
+
           draggedSprite.collide(rectShape)
           draggedSprite.collide(circleShape)
 
-          if(draggedSprite.overlap(triangleShape)) {
-            draggedSprite.position.x = p.windowWidth / 4
-            draggedSprite.position.y = p.windowHeight / 2
+          draggedSprite.overlap(triangleShape, function (e) {
+            e.velocity.x = 0
+            e.velocity.y = 0
+
+            e.position.y = triangleShape.position.y
+            e.position.x = triangleShape.position.x
+
             draggedSprite = null
-          }
+          })
+
           break
         case 'circle':
-          draggedSprite.position.x = p.mouseX
-          draggedSprite.position.y = p.mouseY
+          draggedSprite.velocity.x = (p.mouseX - draggedSprite.position.x) * 0.2;
+          draggedSprite.velocity.y = (p.mouseY - draggedSprite.position.y) * 0.2;
+
           draggedSprite.collide(rectShape)
           draggedSprite.collide(triangleShape)
-          if(draggedSprite.overlap(circleShape)) {
-            draggedSprite.position.x = p.windowWidth / 2.2
-            draggedSprite.position.y = p.windowHeight / 2
+
+          draggedSprite.overlap(circleShape, function (e) {
+            e.velocity.x = 0
+            e.velocity.y = 0
+
+            e.position.y = circleShape.position.y
+            e.position.x = circleShape.position.x
+
             draggedSprite = null
-          }
+          })
+
           break
       }
+
+      circleShape.debug = true
+      triangleShape.debug = true
+      rectShape.debug = true
+      rect.debug = true
+      triangle.debug = true
+      circle.debug = true
     }
+
 
     p.drawSprites()
   }
