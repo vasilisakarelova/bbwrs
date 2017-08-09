9d0cfb43372d381f266a5caf3c87c0736fb5d136
vasilisa <igorevna.vasilisa@gmail.com>
2017-08-09 23:54:54 +0300

    add rotation (UX)

--- build/js/common.js
+++ build/js/common.js
@@ -54567,6 +54567,7 @@ function uxInit(p) {
     }
 
     triangle = p.createSprite(p.random(80, p.windowWidth - 80), p.random(80, p.windowHeight - 80), 100, 100)
+    triangle.rotation = p.random(90)
     triangle.draw = function() {
       p.push()
       p.fill('#417ce6')
@@ -54584,6 +54585,7 @@ function uxInit(p) {
     }
 
     rect = p.createSprite(p.random(160, p.windowWidth - 160), p.random(160, p.windowHeight - 160), 100, 100)
+    rect.rotation = p.random(90)
     rect.draw = function() {
       p.push()
       p.fill('#ff3600')
@@ -54653,6 +54655,8 @@ function uxInit(p) {
             e.position.y = rectShape.position.y
             e.position.x = rectShape.position.x
 
+            e.rotation = 0
+
             draggedSprite = null
           })
 
@@ -54670,6 +54674,8 @@ function uxInit(p) {
 
             e.position.y = triangleShape.position.y
             e.position.x = triangleShape.position.x
+
+            e.rotation = 0
 
             draggedSprite = null
           })
